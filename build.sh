ef137cb1b721ae7c642382daff9bad4d951234c4
Giacomo Zanatta <giacomozanatta@Giacomos-MacBook-Pro.local>
2022-12-02 14:48:52 +0100

    test Dockerfile

--- build.sh
+++ build.sh
@@ -2,9 +2,14 @@ ls -a
 mkdir bin
 go build -o bin/sgotify
 #RUN cd ..
-#RUN cp -R src/client/css app/client/css
+mkdir bin/client
+cp -R client/css bin/client/css
+cp -R client/templates bin/client/templates
+mkdir bin/client/scripts
 #RUN cp -R src/client/templates app/client/templates
 gopherjs build client/scripts/main.go
+cp client/scripts/main.js bin/client/scripts/main.js
+cp client/scripts/main.js.map bin/client/scripts/main.js.map
 #RUN ls -a
 #RUN cd client/scripts
 ls -a
