202fa3c147f56035577d9d01d0742422e1ea4e4e
Giacomo Zanatta <giacomo12596@gmail.com>
2022-12-20 11:34:44 +0100

    Fix grpc, css
    Fix grpc, css

--- sgotipy/sgotipy.pb.go
+++ sgotipy/sgotipy.pb.go
@@ -274,7 +274,10 @@ type SgotipyStatusResponse struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	Status string `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty"`
+	Status       string `protobuf:"bytes,1,opt,name=status,proto3" json:"status,omitempty"`
+	Device       string `protobuf:"bytes,2,opt,name=device,proto3" json:"device,omitempty"`
+	DeviceStatus string `protobuf:"bytes,3,opt,name=device_status,json=deviceStatus,proto3" json:"device_status,omitempty"`
+	CurrentSong  *Song  `protobuf:"bytes,4,opt,name=current_song,json=currentSong,proto3" json:"current_song,omitempty"`
 }
 
 func (x *SgotipyStatusResponse) Reset() {
@@ -316,6 +319,90 @@ func (x *SgotipyStatusResponse) GetStatus() string {
 	return ""
 }
 
+func (x *SgotipyStatusResponse) GetDevice() string {
+	if x != nil {
+		return x.Device
+	}
+	return ""
+}
+
+func (x *SgotipyStatusResponse) GetDeviceStatus() string {
+	if x != nil {
+		return x.DeviceStatus
+	}
+	return ""
+}
+
+func (x *SgotipyStatusResponse) GetCurrentSong() *Song {
+	if x != nil {
+		return x.CurrentSong
+	}
+	return nil
+}
+
+type Song struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id      string `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Title   string `protobuf:"bytes,2,opt,name=title,proto3" json:"title,omitempty"`
+	Artists string `protobuf:"bytes,3,opt,name=artists,proto3" json:"artists,omitempty"`
+}
+
+func (x *Song) Reset() {
+	*x = Song{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_sgotipy_proto_msgTypes[6]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Song) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Song) ProtoMessage() {}
+
+func (x *Song) ProtoReflect() protoreflect.Message {
+	mi := &file_sgotipy_proto_msgTypes[6]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Song.ProtoReflect.Descriptor instead.
+func (*Song) Descriptor() ([]byte, []int) {
+	return file_sgotipy_proto_rawDescGZIP(), []int{6}
+}
+
+func (x *Song) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *Song) GetTitle() string {
+	if x != nil {
+		return x.Title
+	}
+	return ""
+}
+
+func (x *Song) GetArtists() string {
+	if x != nil {
+		return x.Artists
+	}
+	return ""
+}
+
 var File_sgotipy_proto protoreflect.FileDescriptor
 
 var file_sgotipy_proto_rawDesc = []byte{
@@ -340,10 +427,21 @@ var file_sgotipy_proto_rawDesc = []byte{
 	0x73, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x18, 0x01, 0x20,
 	0x01, 0x28, 0x09, 0x52, 0x07, 0x6d, 0x65, 0x73, 0x73, 0x61, 0x67, 0x65, 0x22, 0x16, 0x0a, 0x14,
 	0x53, 0x67, 0x6f, 0x74, 0x69, 0x70, 0x79, 0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x52, 0x65, 0x71,
-	0x75, 0x65, 0x73, 0x74, 0x22, 0x2f, 0x0a, 0x15, 0x53, 0x67, 0x6f, 0x74, 0x69, 0x70, 0x79, 0x53,
-	0x74, 0x61, 0x74, 0x75, 0x73, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x16, 0x0a,
-	0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x73,
-	0x74, 0x61, 0x74, 0x75, 0x73, 0x32, 0xc6, 0x01, 0x0a, 0x07, 0x53, 0x67, 0x6f, 0x74, 0x69, 0x70,
+	0x75, 0x65, 0x73, 0x74, 0x22, 0x96, 0x01, 0x0a, 0x15, 0x53, 0x67, 0x6f, 0x74, 0x69, 0x70, 0x79,
+	0x53, 0x74, 0x61, 0x74, 0x75, 0x73, 0x52, 0x65, 0x73, 0x70, 0x6f, 0x6e, 0x73, 0x65, 0x12, 0x16,
+	0x0a, 0x06, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06,
+	0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x12, 0x16, 0x0a, 0x06, 0x64, 0x65, 0x76, 0x69, 0x63, 0x65,
+	0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52, 0x06, 0x64, 0x65, 0x76, 0x69, 0x63, 0x65, 0x12, 0x23,
+	0x0a, 0x0d, 0x64, 0x65, 0x76, 0x69, 0x63, 0x65, 0x5f, 0x73, 0x74, 0x61, 0x74, 0x75, 0x73, 0x18,
+	0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0c, 0x64, 0x65, 0x76, 0x69, 0x63, 0x65, 0x53, 0x74, 0x61,
+	0x74, 0x75, 0x73, 0x12, 0x28, 0x0a, 0x0c, 0x63, 0x75, 0x72, 0x72, 0x65, 0x6e, 0x74, 0x5f, 0x73,
+	0x6f, 0x6e, 0x67, 0x18, 0x04, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x05, 0x2e, 0x53, 0x6f, 0x6e, 0x67,
+	0x52, 0x0b, 0x63, 0x75, 0x72, 0x72, 0x65, 0x6e, 0x74, 0x53, 0x6f, 0x6e, 0x67, 0x22, 0x46, 0x0a,
+	0x04, 0x53, 0x6f, 0x6e, 0x67, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64, 0x18, 0x01, 0x20, 0x01, 0x28,
+	0x09, 0x52, 0x02, 0x69, 0x64, 0x12, 0x14, 0x0a, 0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x18, 0x02,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x74, 0x69, 0x74, 0x6c, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x61,
+	0x72, 0x74, 0x69, 0x73, 0x74, 0x73, 0x18, 0x03, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x61, 0x72,
+	0x74, 0x69, 0x73, 0x74, 0x73, 0x32, 0xc6, 0x01, 0x0a, 0x07, 0x53, 0x67, 0x6f, 0x74, 0x69, 0x70,
 	0x79, 0x12, 0x3d, 0x0a, 0x0c, 0x53, 0x74, 0x61, 0x72, 0x74, 0x53, 0x67, 0x6f, 0x74, 0x69, 0x70,
 	0x79, 0x12, 0x14, 0x2e, 0x53, 0x74, 0x61, 0x72, 0x74, 0x53, 0x67, 0x6f, 0x74, 0x69, 0x70, 0x79,
 	0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x1a, 0x15, 0x2e, 0x53, 0x74, 0x61, 0x72, 0x74, 0x53,
@@ -372,7 +470,7 @@ func file_sgotipy_proto_rawDescGZIP() []byte {
 	return file_sgotipy_proto_rawDescData
 }
 
-var file_sgotipy_proto_msgTypes = make([]protoimpl.MessageInfo, 6)
+var file_sgotipy_proto_msgTypes = make([]protoimpl.MessageInfo, 7)
 var file_sgotipy_proto_goTypes = []interface{}{
 	(*StartSgotipyRequest)(nil),   // 0: StartSgotipyRequest
 	(*StartSgotipyResponse)(nil),  // 1: StartSgotipyResponse
@@ -380,19 +478,21 @@ var file_sgotipy_proto_goTypes = []interface{}{
 	(*StopSgotipyResponse)(nil),   // 3: StopSgotipyResponse
 	(*SgotipyStatusRequest)(nil),  // 4: SgotipyStatusRequest
 	(*SgotipyStatusResponse)(nil), // 5: SgotipyStatusResponse
+	(*Song)(nil),                  // 6: Song
 }
 var file_sgotipy_proto_depIdxs = []int32{
-	0, // 0: Sgotipy.StartSgotipy:input_type -> StartSgotipyRequest
-	2, // 1: Sgotipy.StopSgotipy:input_type -> StopSgotipyRequest
-	4, // 2: Sgotipy.SgotipyStatus:input_type -> SgotipyStatusRequest
-	1, // 3: Sgotipy.StartSgotipy:output_type -> StartSgotipyResponse
-	3, // 4: Sgotipy.StopSgotipy:output_type -> StopSgotipyResponse
-	5, // 5: Sgotipy.SgotipyStatus:output_type -> SgotipyStatusResponse
-	3, // [3:6] is the sub-list for method output_type
-	0, // [0:3] is the sub-list for method input_type
-	0, // [0:0] is the sub-list for extension type_name
-	0, // [0:0] is the sub-list for extension extendee
-	0, // [0:0] is the sub-list for field type_name
+	6, // 0: SgotipyStatusResponse.current_song:type_name -> Song
+	0, // 1: Sgotipy.StartSgotipy:input_type -> StartSgotipyRequest
+	2, // 2: Sgotipy.StopSgotipy:input_type -> StopSgotipyRequest
+	4, // 3: Sgotipy.SgotipyStatus:input_type -> SgotipyStatusRequest
+	1, // 4: Sgotipy.StartSgotipy:output_type -> StartSgotipyResponse
+	3, // 5: Sgotipy.StopSgotipy:output_type -> StopSgotipyResponse
+	5, // 6: Sgotipy.SgotipyStatus:output_type -> SgotipyStatusResponse
+	4, // [4:7] is the sub-list for method output_type
+	1, // [1:4] is the sub-list for method input_type
+	1, // [1:1] is the sub-list for extension type_name
+	1, // [1:1] is the sub-list for extension extendee
+	0, // [0:1] is the sub-list for field type_name
 }
 
 func init() { file_sgotipy_proto_init() }
@@ -473,6 +573,18 @@ func file_sgotipy_proto_init() {
 				return nil
 			}
 		}
+		file_sgotipy_proto_msgTypes[6].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Song); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -480,7 +592,7 @@ func file_sgotipy_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_sgotipy_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   6,
+			NumMessages:   7,
 			NumExtensions: 0,
 			NumServices:   1,
 		},
